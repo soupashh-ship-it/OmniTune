9cf306e223d55abe3a3133523eed29fdb6733924
soupashh-ship-it <292704455+soupashh-ship-it@users.noreply.github.com>
2026-09-10 16:41:58 +0530

    chore: bump prerelease version to pre8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,8 +50,8 @@ android {
         applicationId = "com.omnitune.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 125
-        versionName = "1.2.0-pre7"
+        versionCode = 126
+        versionName = "1.2.0-pre8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
